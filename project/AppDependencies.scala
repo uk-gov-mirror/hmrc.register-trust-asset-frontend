0857e076cbdb1cb3a5a56654bd52f9385a013e29
mahesh <175945803+maheshhvr@users.noreply.github.com>
2026-07-23 14:28:22 +0100

    DDCE-8580 Upgrade play-frontend-hmrc in register-trust-asset-frontend (#152)
    * Upgrade play-frontend-hmrc in register-trust-asset-frontend
    
    * DDCE-8580 update deps ,delete unused html

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ object AppDependencies {
   val bootstrapVersion = "10.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.20.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % Test)
 
